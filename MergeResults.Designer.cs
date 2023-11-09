8915303e5c2818af8b63c2df72ac40b58cab80d8
JOMacgilp <JOMacgilp@W-006137>
2023-11-09 15:46:40 +0000

    Added Merge functionality, needs further testing and work

---
+++ MergeResults.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SwordJet
+{
+    partial class MergeResults
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvResults = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConfirm.Location = new System.Drawing.Point(578, 644);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(132, 37);
+            this.btnConfirm.TabIndex = 0;
+            this.btnConfirm.Text = "Confirm Merge";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(716, 644);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(132, 37);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.AllowUserToAddRows = false;
+            this.dgvResults.AllowUserToDeleteRows = false;
+            this.dgvResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.Location = new System.Drawing.Point(12, 12);
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.RowTemplate.Height = 24;
+            this.dgvResults.Size = new System.Drawing.Size(858, 626);
+            this.dgvResults.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 644);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(560, 37);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "If the Merge is confirmed, the results in the tournaments will be merged. This pr" +
+    "ocess cannot be reversed, so a backup .sjt file is advised";
+            // 
+            // MergeResults
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 693);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgvResults);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConfirm);
+            this.Name = "MergeResults";
+            this.Text = "MergeResults";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dgvResults;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ MergeResults.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwordJet
+{
+    public partial class MergeResults : Form
+    {
+        private Tournament firstTournament, secondTournament;
+        public List<Fight> updates;
+
+        public MergeResults(Tournament a, Tournament b)
+        {
+            InitializeComponent();
+
+            if(!Tournament.DoTournamentsMatch(a,b))
+            {
+                MessageBox.Show("Loaded tournament files do not match, results cannot be merged");
+                Close();
+            }
+
+            firstTournament = a;
+            secondTournament = b;
+            updates = new List<Fight>();
+
+            LoadFightList();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            updates.Clear();
+            foreach(DataRow row in ((DataTable)dgvResults.DataSource).Rows)
+            {
+                if((bool)row["Merge?"] == true)
+                {
+                    updates.Add(secondTournament.FindFight((Guid)row["Fight ID"]));
+                }
+            }
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        public void LoadFightList()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Fight ID", typeof(string));
+            table.Columns.Add("Fighter A", typeof(string));
+            table.Columns.Add("Fighter B", typeof(string));
+            table.Columns.Add("Pool", typeof(string));
+            table.Columns.Add("Fighter A Result (First Tournament)", typeof(string));
+            table.Columns.Add("Fighter B Result (First Tournament)", typeof(string));
+            table.Columns.Add("Doubles (First Tournament)", typeof(string));
+            table.Columns.Add("Fighter A Result (Second Tournament)", typeof(string));
+            table.Columns.Add("Fighter B Result (Second Tournament)", typeof(string));
+            table.Columns.Add("Doubles (Second Tournament)", typeof(string));
+            table.Columns.Add("Merge?", typeof(bool));
+
+            for(int i = 0; i < table.Columns.Count-1; i++)
+            {
+                table.Columns[i].ReadOnly = true;
+            }
+
+            for(int i = 0; i < firstTournament.pools.Count; i++)
+            {
+                Pool p = firstTournament.pools[i];
+                for(int j = 0; j < p.rounds.Count; j++)
+                {
+                    List<Fight> round = p.rounds[j];
+
+                    for(int k = 0; k < round.Count; k++)
+                    {
+                        Fight a = firstTournament.pools[i].rounds[j][k];
+                        Fight b = secondTournament.pools[i].rounds[j][k];
+
+                        if (a.fighterAResult != b.fighterAResult || a.fighterBResult != b.fighterBResult || a.doubleCount != b.doubleCount)
+                        {
+                            DataRow row = table.NewRow();
+
+                            row["Fight ID"] = a.fightID;
+                            row["Fighter A"] = firstTournament.GetFighterByID(a.fighterA).name;
+                            row["Fighter B"] = firstTournament.GetFighterByID(a.fighterB).name;
+                            row["Pool"] = p.name;
+                            row["Fighter A Result (First Tournament)"] = a.fighterAResult.ToString();
+                            row["Fighter B Result (First Tournament)"] = a.fighterBResult.ToString();
+                            row["Doubles (First Tournament)"] = a.doubleCount;
+                            row["Fighter A Result (Second Tournament)"] = b.fighterAResult.ToString();
+                            row["Fighter B Result (Second Tournament)"] = b.fighterBResult.ToString();
+                            row["Doubles (Second Tournament)"] = b.doubleCount;
+                            row["Merge?"] = true;
+
+                            table.Rows.Add(row);
+                        }
+                    }
+                }
+            }
+
+            dgvResults.DataSource = table;
+        }
+    }
+}
